e2cabdc6e30694b875a282ed5b27445cc1a72b5a
johnniesimpson <johnniesimpson@outlook.com>
2021-12-28 12:00:31 -0500

    Increase tip options.

--- tip.py
+++ tip.py
@@ -1,6 +1,6 @@
 print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill? $"))
-tip = input("How much tip would you like to give? 10, 12, or 15? ")
+tip = input("How much tip would you like to give? 12, 15, or 20? ")
 people = int(input("how many people to split the bill? "))
 
 tip_num = (int(tip) / 100) + 1
